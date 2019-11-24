426f543d3ed83a3d81798ae29f730e10c10ab601
magamboatec <magamboa@ic-itcr.ac.cr>
2019-11-24 14:04:38 -0600

    Update ConsumableFactory.java
    getRandomMeal
    getRandomDrug

--- PrograGoku/src/ADT/ConsumableFactory.java
+++ PrograGoku/src/ADT/ConsumableFactory.java
@@ -3,6 +3,8 @@ package ADT;
 import java.util.HashMap;
 import java.util.Random;
 
+import abstraction.ADrug;
+import abstraction.AMeal;
 import abstraction.IConsumable;
 
 public class ConsumableFactory {
@@ -21,4 +23,26 @@ public class ConsumableFactory {
 		int num = new Random().nextInt(qKeys);
 		return consumables.get(consumables.keySet().toArray()[num]);
 	}
+	
+	public AMeal getRandomMeal(){
+		int qKeys = consumables.size();
+		IConsumable consumable = null;
+		int num;
+		while(!(consumable instanceof AMeal)) {
+			num = new Random().nextInt(qKeys);
+			consumable = consumables.get(consumables.keySet().toArray()[num]);
+		}
+		return (AMeal) consumable;
+	}	
+	
+	public ADrug getRandomDrug(){
+		int qKeys = consumables.size();
+		IConsumable consumable = null;
+		int num;
+		while(!(consumable instanceof ADrug)) {
+			num = new Random().nextInt(qKeys);
+			consumable = consumables.get(consumables.keySet().toArray()[num]);
+		}
+		return (ADrug) consumable;
+	}		
 }

3814812ff777a253c6cd25ca2a54cb5ab4c69618
Carlos Esquivel <carlosrem70@gmail.com>
2019-11-26 13:51:18 -0600

    Forgot about AConsumable, my bad

--- PrograGoku/src/abstraction/AConsumable.java
+++ PrograGoku/src/abstraction/AConsumable.java
@@ -1,7 +1,18 @@
 package abstraction;
 
-import ADT.ExtendedCharacter;
-
-public interface IConsumable extends IVisitor {
+public abstract class AConsumable implements IVisitor {
 	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public AConsumable(String name) {
+		this.name = name;
+	}
 }

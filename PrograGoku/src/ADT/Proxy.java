8b27f8b7c35cec0fcba83afd3f376adc6bfe4aca
magamboatec <magamboa@ic-itcr.ac.cr>
2019-11-25 20:25:08 -0600

    Update Proxy.java
    generate file

--- PrograGoku/src/ADT/Proxy.java
+++ PrograGoku/src/ADT/Proxy.java
@@ -1,5 +1,27 @@
 package ADT;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
 public class Proxy {
 	public static StringBuilder log = new StringBuilder();	
+	
+	public static void generateFile() {
+	       SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss_z");
+	        Date date = new Date(System.currentTimeMillis());
+	        
+            String userHomeFolder = System.getProperty("user.home");
+            String fileSeparator = System.getProperty("file.separator");
+            String absoluteFilePath = userHomeFolder+fileSeparator+formatter.format(date)+".json";
+            PrintWriter writer;
+			try {
+				writer = new PrintWriter(new File(absoluteFilePath));
+				writer.write(log.toString());
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+	}
 }
